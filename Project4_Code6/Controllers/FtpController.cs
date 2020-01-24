3a4ad1c9a598575364af26cdf231dbfd2afea0aa
aswevers <amber.swevers@hotmail.com>
2020-01-24 09:27:08 +0100

    Backend fix and second livestream

--- Project4_Code6/Controllers/FtpController.cs
+++ Project4_Code6/Controllers/FtpController.cs
@@ -27,7 +27,7 @@ namespace Project4_Code6.Controllers
         }
 
         [HttpGet]
-        public FileStreamResult GetPhoto()
+        public FileStreamResult GetPhotoHal()
         {
             List<string> files = getFilesnames();
             string To = "ftp://192.168.1.82/ftp/files/FI9803EP_00626E66ABD9/snap/" + files[files.Count()-1];
@@ -63,10 +63,74 @@ namespace Project4_Code6.Controllers
             
 
         }
-        [HttpGet("upload")]
+
+        [HttpGet("dome")]
+        public FileStreamResult GetPhotoDome()
+        {
+
+            List<string> files = getFilesnamesDome();
+            string To = "ftp://pi@192.168.1.82/ftp/files/FI9853EP_00626E61750D/snap/" + files[files.Count() - 1];
+
+
+            try
+            {
+                /* Create an FTP Request */
+                var ftpRequest = (FtpWebRequest)FtpWebRequest.Create(To);
+                /* Log in to the FTP Server with the User Name and Password Provided */
+                ftpRequest.Credentials = new NetworkCredential(UserId, Password);
+                /* When in doubt, use these options */
+                ftpRequest.UseBinary = true;
+                ftpRequest.UsePassive = true;
+                ftpRequest.KeepAlive = true;
+                /* Specify the Type of FTP Request */
+                ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                /* Establish Return Communication with the FTP Server */
+                var ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                /* Get the FTP Server's Response Stream */
+                var ftpStream = ftpResponse.GetResponseStream();
+
+                // TODO: you might need to extract these settings from the FTP response
+                const string contentType = "application/jpg";
+                string fileNameDisplayedToUser = files[files.Count() - 1];
+
+                return File(ftpStream, contentType, fileNameDisplayedToUser);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+        [HttpGet("filesnameHal")]
         public List<string> getFilesnames()
         {
             FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(ParentFolderPath);
+            ftpRequest.Credentials = new NetworkCredential(UserId, Password);
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+            FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+            List<string> directories = new List<string>();
+
+            string line = streamReader.ReadLine();
+            while (!string.IsNullOrEmpty(line))
+            {
+                var lineArr = line.Split('/');
+                line = lineArr[lineArr.Count() - 1];
+                directories.Add(line);
+                line = streamReader.ReadLine();
+            }
+
+            streamReader.Close();
+
+            return directories;
+        }
+
+        [HttpGet("filesnamesDome")]
+        public List<string> getFilesnamesDome()
+        {
+            FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create("ftp://pi@192.168.1.82/ftp/files/FI9853EP_00626E61750D/snap/");
             ftpRequest.Credentials = new NetworkCredential(UserId, Password);
             ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
             FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
